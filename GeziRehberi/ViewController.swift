5e80931541627d84233928455a7ca5c7220ccdfb
bulentkarsli <bulentkarsli@outlook.com>
2019-10-31 20:05:43 +0300

    Initial Commit

---
+++ GeziRehberi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeziRehberi
+//
+//  Created by KARSLI on 31.10.2019.
+//  Copyright © 2019 Bülent KARSLI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
